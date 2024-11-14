47656a12f5de2ca10bbe54047ce2c395d9f3c921
coreybrowndev <bcorey395@gmail.com>
2024-11-13 23:47:29 -0500

    Update page

--- src/app/videos/video/add/page.tsx
+++ src/app/videos/video/add/page.tsx
@@ -1,27 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
 export default function AddVideoPage() {
-  const handleCreateVideo = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-
-    prisma.video.create({
-      data: {
-        name: formData.get("name") as string,
-        url: formData.get("url") as string,
-        votes: parseInt(formData.get("votes") as string),
-        length: parseInt(formData.get("length") as string),
-      },
-    });
-  };
-
   return (
     <div>
       <h1>Add New Video</h1>
-      <form onSubmit={(e) => handleCreateVideo(e)} method="POST">
+      <form method="POST">
         <input type="text" placeholder="Name" />
         <input type="text" placeholder="url" />
         <input type="number" placeholder="votes" />
